2e8132987c2a228ef0424b124bae96ba8ebedf8f
Ekam-Momi <ekam.m27@gmail.com>
2023-08-04 00:21:13 -0400

    working on the project

--- YtDownloader.py
+++ YtDownloader.py
@@ -1,3 +1,30 @@
 import tkinter
 import customtkinter
 from pytube import YouTube
+
+# Settings
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+# App frame
+my_app = customtkinter.CTk()
+my_app.geometry("740x480")
+my_app.title("Youtube Video Downloader")
+
+                                        # USER INTERFACE ELEMENTS
+
+# Instructional title
+heading = customtkinter.CTkLabel(my_app, text = "Enter YouTube link below")
+heading.pack(padx = "10", pady = "10")
+
+# Link input
+url_var = tkinter.StringVar()
+link = customtkinter.CTkEntry(my_app, width = 400, height = 40, textvariable = url_var)
+link.pack()
+
+# Button for downloading
+
+
+
+# Running the app
+my_app.mainloop()
