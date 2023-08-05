cdf847a5dbd0168996bc25895c82daab494f91c1
Ekam-Momi <ekam.m27@gmail.com>
2023-08-05 02:39:48 -0400

    Working on the project

--- YtDownloader.py
+++ YtDownloader.py
@@ -2,6 +2,19 @@ import tkinter
 import customtkinter
 from pytube import YouTube
 
+# This function will start the downloading of the youtube video
+def start_download():
+    try:
+        yT_link = link.get()
+        ytObj = YouTube(yT_link)
+        video = ytObj.streams.get_highest_resolution()
+        video.download()
+    except:
+        print("There seems to be an error :(. ")
+    print("Download Complete.")
+
+
+
 # Settings
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
@@ -23,8 +36,8 @@ link = customtkinter.CTkEntry(my_app, width = 400, height = 40, textvariable = u
 link.pack()
 
 # Button for downloading
-
-
+dnld_btn = customtkinter.CTkButton(my_app, text = "Download", command = start_download)
+dnld_btn.pack(padx = 10, pady = 10)
 
 # Running the app
 my_app.mainloop()
