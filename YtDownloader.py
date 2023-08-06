bfd00432ab6c3c6822a9e122d6fd3df63072771a
Ekam-Momi <ekam.m27@gmail.com>
2023-08-06 01:15:38 -0400

    working on the project

--- YtDownloader.py
+++ YtDownloader.py
@@ -2,21 +2,24 @@ import tkinter
 import customtkinter
 from pytube import YouTube
 
+                                    #FUNCTIONS
+
 # This function will start the downloading of the youtube video
 def start_download():
     try:
         yT_link = link.get()
         ytObj = YouTube(yT_link)
+        heading.configure(text  = ytObj.title)
         video = ytObj.streams.get_highest_resolution()
+        fin_lable.configure(text = "")
         video.download()
+        fin_lable.configure(text = "Download complete.", text_color = "green")
     except:
-        print("There seems to be an error :(. ")
-    print("Download Complete.")
-
-
+        fin_lable.configure(text = "Invalid link, video could not be downloaded.", text_color = "red")
+    
 
 # Settings
-customtkinter.set_appearance_mode("System")
+customtkinter.set_appearance_mode("System") 
 customtkinter.set_default_color_theme("blue")
 
 # App frame
@@ -35,9 +38,13 @@ url_var = tkinter.StringVar()
 link = customtkinter.CTkEntry(my_app, width = 400, height = 40, textvariable = url_var)
 link.pack()
 
-# Button for downloading
+# Finished message for user
+fin_lable = customtkinter.CTkLabel(my_app, text = "")
+fin_lable.pack(padx = "10", pady = "10")
+
+# Button for downloading video in maximum resoulution
 dnld_btn = customtkinter.CTkButton(my_app, text = "Download", command = start_download)
-dnld_btn.pack(padx = 10, pady = 10)
+dnld_btn.pack(padx = "10", pady = "10")
 
 # Running the app
 my_app.mainloop()
